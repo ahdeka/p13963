06181141c81d9dec3e48684b64aca2a5727875d6
ahdeka <ahdeka01@gmail.com>
2025-06-18 15:23:23 +0900

    3강 Bean 등록

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,6 +1,13 @@
 package com.back;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PersonService {
+    private final PersonRepository personRepository;
+
     public long count() {
         return 3;
     }
