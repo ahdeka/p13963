1d2a96dbf0d78a6ed7b7ec160a8af3a56d1d75f5
ahdeka <ahdeka01@gmail.com>
2025-06-19 15:53:01 +0900

    22강 도메인별 분리

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,13 +1,21 @@
 package com.back;
 
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+
+    private final MemberService memberService;
+
 
     @Autowired
     @Lazy
@@ -19,27 +27,29 @@ public class AppConfig {
     }
 
     @Bean
-    ApplicationRunner myApplicationRunner() {
-        return args -> System.out.println("MyApplicationRunner is running with args: " + args.getSourceArgs().length);
-    }
-
-
-    @Bean
-    ApplicationRunner myApplicationRunner2() {
+    ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            self.work1();
-            self.work2();
+            this.work1();
+            this.work2();
         };
     }
 
-
+    @Transactional
     private void work1() {
-        System.out.println("work1 is running");
+        if (memberService.count() > 0) return;
+
+        Member memberSystem = memberService.join("system", "1234", "시스템");
+        Member memberAdmin = memberService.join("admin", "1234", "관리자");
+        Member memberUser1 = memberService.join("user1", "1234", "사용자1");
+        Member memberUser2 = memberService.join("user2", "1234", "사용자2");
+        Member memberUser3 = memberService.join("user3", "1234", "사용자3");
     }
 
-
+    @Transactional
     private void work2() {
-        System.out.println("work2 is running");
+        Member member2 = memberService.findByUsername("user2").get();
+
+        member2.setNickname("유저2 New");
     }
 
 }
